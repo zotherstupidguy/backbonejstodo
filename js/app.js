17c8c698cc73a35f6b0963e036fea1c6979d1b7e
zotherstupidguy <zotherstupidguy@gmail.com>
2016-03-31 13:27:39 +0200

    wrapping

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
- var app = app || {};
-  var ENTER_KEY = 13;
+var app = app || {};
+var ENTER_KEY = 13;
 
-  $(function() {
+$(function() {
 
-    // Kick things off by creating the **App**.
-    new app.AppView();
+  // Kick things off by creating the **App**.
+  new app.AppView();
 
-  });
+});
--- js/models/todo.js
+++ js/models/todo.js
@@ -1,24 +1,25 @@
- // js/models/todo.js
+// js/models/todo.js
 
-  var app = app || {};
+var app = app || {};
 
-  // Todo Model
-  // ----------
-  // Our basic **Todo** model has `title`, `order`, and `completed` attributes.
+// Todo Model
+// ----------
+// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
 
-  app.Todo = Backbone.Model.extend({
+app.Todo = Backbone.Model.extend({
 
-    // Default attributes ensure that each todo created has `title` and `completed` keys.
-    defaults: {
-      title: '',
-      completed: false
-    },
+  // Default attributes ensure that each todo created has `title` and `completed` keys.
+  defaults: {
+    title: '',
+  //order: Todos.nextorder(),
+  completed: false
+  },
 
-    // Toggle the `completed` state of this todo item.
-    toggle: function() {
-      this.save({
-        completed: !this.get('completed')
-      });
-    }
+  // Toggle the `completed` state of this todo item.
+  toggle: function() {
+    this.save({
+      completed: !this.get('completed')
+    });
+  }
 
-  });
+});
