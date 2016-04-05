0c5ec8ef350dc891e90d30df3ac09b0ebf1ab28b
zotherstupidguy <zotherstupidguy@gmail.com>
2016-04-05 21:16:22 +0200

    nada

--- js/routers/router.js
+++ js/routers/router.js
@@ -1,17 +1,17 @@
 // js/routers/router.js
 
-  // Todo Router
-  // ----------
+// Todo Router
+// ----------
 
-  var Workspace = Backbone.Router.extend({
-    routes:{
-      '*filter': 'setFilter'
-    },
+var Workspace = Backbone.Router.extend({
+  routes:{
+    '*filter': 'setFilter'
+  },
 
     setFilter: function( param ) {
       // Set the current filter to be used
       if (param) {
-        param = param.trim();
+	param = param.trim();
       }
       app.TodoFilter = param || '';
 
@@ -19,7 +19,7 @@
       // of Todo view items
       app.Todos.trigger('filter');
     }
-  });
+});
 
-  app.TodoRouter = new Workspace();
-  Backbone.history.start();
+app.TodoRouter = new Workspace();
+Backbone.history.start();
